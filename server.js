0d83b4a75d03508ce7fdc7b2895aa6abca7afb89
xScoopy <JLeary070@gmail.com>
2021-04-12 15:42:40 -0500

    added sections for requirements, setup, middleware, and routes

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+// Library requirements
 const express = require('express')
+
+//app setup
 const app = express()
 const port = 3000
 
+//Middleware
+
+
+
+//routes
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+
+//Server start
 app.listen(port, () => {
     console.log(`Example app listening at port ${port}`)
 })
