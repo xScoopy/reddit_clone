1a69c34a107872ece53c0032115b29a3326dfe9f
xScoopy <JLeary070@gmail.com>
2021-04-12 18:21:29 -0500

    Users can submit and save posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,6 +2,12 @@ const Post = require('../models/post');
 
 module.exports = app => {
     app.post("/posts/new", (req, res) => {
-        console.log(req.body);
-    })
-}
+        const post = new Post(req.body);
+        
+        //Save instance of post model to db
+        post.save((err, post) => {
+            //redirect to root
+            return res.redirect('/');
+        })
+    });
+};
