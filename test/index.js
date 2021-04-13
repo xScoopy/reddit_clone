5eddc6ee102d1fa7713ed29d22abcda10a503f31
xScoopy <JLeary070@gmail.com>
2021-04-13 17:04:31 -0500

    created index.js in test folder, added chai and mocha functionality

---
+++ test/index.js
@@ -0,0 +1,24 @@
+const app = require("./../server");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const should = shai.should();
+
+chai.use(chaiHttp);
+
+describe("site", function() {
+    //describe what we are testing
+    it("Should have home page", function(done) {
+        //describe what should happen
+        //in this case we test that the home page loads
+        chai 
+        .request(app)
+        .get("/")
+        .end(function(err,res) {
+            if (err) {
+                return done(err);
+            }
+            res.status.should.be.equal(200);
+            return done(); //call done if the test completed successfully
+        })
+    })
+})
