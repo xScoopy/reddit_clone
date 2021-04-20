edeff8ed8435c0ca4baf76eb4e5db56033c422c0
xScoopy <JLeary070@gmail.com>
2021-04-20 13:45:04 -0500

    users can now log in

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,10 +8,44 @@ module.exports = (app) => {
         res.render("sign-up");
     });
     //LOGOUT GET
-    app.get('/logout', (req,res) => {
+    app.get('/logout', (req, res) => {
         res.clearCookie('nToken');
         res.redirect('/');
     })
+    //Login GET
+    app.get('/login', (req, res) => {
+        res.render('login');
+    })
+    //LOGIN POST
+    app.post("/login", (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+        // Find this user name
+        User.findOne({ username }, "username password")
+            .then(user => {
+                if (!user) {
+                    // User not found
+                    return res.status(401).send({ message: "Wrong Username or Password" });
+                }
+                // Check the password
+                user.comparePassword(password, (err, isMatch) => {
+                    if (!isMatch) {
+                        // Password does not match
+                        return res.status(401).send({ message: "Wrong Username or password" });
+                    }
+                    // Create a token
+                    const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+                        expiresIn: "60 days"
+                    });
+                    // Set a cookie and redirect to root
+                    res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+                    res.redirect("/");
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
     // SIGN UP POST
     app.post("/sign-up", (req, res) => {
         // Create User
@@ -20,13 +54,13 @@ module.exports = (app) => {
         user
             .save()
             .then((user) => {
-                var token = jwt.sign({ _id: user._id }, process.env.SECRET, {expiresIn: "60 days" });
-                res.cookie('nToken', token, {maxAge: 900000, httpOnly: true });
+                var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+                res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
                 res.redirect("/");
             })
             .catch(err => {
                 console.log(err.message);
-                return res.status(400).send({err:err});
+                return res.status(400).send({ err: err });
             });
     });
 }
