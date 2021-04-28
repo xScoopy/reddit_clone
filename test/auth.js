07ee2e3e1b0b973a0329128ea8edc1325aaab432
xScoopy <JLeary070@gmail.com>
2021-04-27 19:09:21 -0500

    added auth.js for testing

---
+++ test/auth.js
@@ -0,0 +1,14 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../server");
+const should = chai.should();
+chai.use(chaiHttp);
+
+// Agent that will keep track of our cookies
+const agent = chai.request.agent(server);
+
+const User = require("../models/user");
+
+describe("User", function() {
+  // TESTS WILL GO HERE.
+});
